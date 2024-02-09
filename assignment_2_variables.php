c9fd96dc7547487f7688d57393d44d4ba4b1bfe2
moamel97 <muamal1997@gmail.com>
2024-02-09 01:49:06 -0800

    first assignment

---
+++ assignment_2_variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignment 2 Variables</title>
+</head>
+<body>
+<?php
+$title = 'Googleplex';
+$streetAddress = '1600 Amphitheater parkway';
+$city = 'Mountain View';
+$state = 'CA';
+$country = 'United States';
+
+echo "<h1>$title</h1>
+      <p>The $title is the corporate headquarters complex of Google and its parent company Alphabet Inc. It is located at: </br>
+       $streetAddress</br>
+      $city, $state, $country </p>";
+?>
+</body>
+</html>
